7b0b7d26fc33700c7cc9b7972cd4399bcfea6a70
Tom Yang <xy2286@columbia.edu>
2019-01-02 15:51:14 -0500

    readplanets

--- proj0/In.java
+++ proj0/In.java
@@ -11,7 +11,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.net.HttpURLConnection;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
